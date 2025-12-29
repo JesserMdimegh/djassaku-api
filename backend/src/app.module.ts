2c3d2285d6e29f187c33fa9fb7b9a8b0ab052d6f
Jesser mdimegh <jessermdimagh@gmail.com>
2025-12-29 17:36:52 +0100

    fix upload issue

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
-// import { UploadModule } from './upload/upload.module';
+import { UploadModule } from './upload/upload.module';
 import { Product } from './products/entities/product.entity';
 import { Order } from './orders/entities/order.entity';
 import { User } from './auth/entities/user.entity';
@@ -24,7 +24,7 @@ import { User } from './auth/entities/user.entity';
     ProductsModule,
     OrdersModule,
     AuthModule,
-    // UploadModule, // Temporarily disabled
+    UploadModule,
   ],
 })
 export class AppModule {}
