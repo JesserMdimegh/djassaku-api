2f3db05dbec0b4b8c76d1c9c352a2c24089742cc
Jesser mdimegh <jessermdimagh@gmail.com>
2026-01-04 13:45:45 +0100

    health checks

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
 import { UploadModule } from './upload/upload.module';
+import { HealthModule } from './health/health.module';
 import { Product } from './products/entities/product.entity';
 import { Order } from './orders/entities/order.entity';
 import { User } from './auth/entities/user.entity';
@@ -25,6 +26,7 @@ import { User } from './auth/entities/user.entity';
     OrdersModule,
     AuthModule,
     UploadModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
